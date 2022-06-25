6292ba176b4f3d8f7fbf5d208c203a10a17bbe9b
jim-llorenz <lautaro.lorenz@mercadolibre.com>
2022-06-25 18:32:51 -0300

    add electron-reload

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-const { initApp } = require('./app')
+const path = require('path');
 
-initApp({ 
+require('electron-reload')(
+  [
+    path.join(__dirname, "app"),
+  ],
+  {
+    electron: path.join(__dirname, 'node_modules', '.bin', 'electron'),
+    forceHardReset: true
+  }
+);
+
+const { initApp } = require('./app');
+
+initApp({
   isProduction: false,
   baseUrl: 'http://localhost:4200',
 })
