43bceace7e849bb6cc60a3b2337804c76032dbb9
jim-llorenz <lautaro.lorenz@mercadolibre.com>
2022-07-28 15:25:57 -0300

    mejoras en el guard de turn off

--- src/app/guards/devices-turn-off.guard.ts
+++ src/app/guards/devices-turn-off.guard.ts
@@ -22,51 +22,58 @@ export class DevicesTurnOffGuard implements CanDeactivate<ComponentCanDeactivate
   ) { }
 
   canDeactivate(): Observable<boolean> {
-    return of(!this.usbHandlerService.connected$.value).pipe(
-      switchMap(() => forkJoin({
-        generatorStatus: of(this.generatorService.generatorStatus$.value),
-        patternStatus: of(this.patternService.patternStatus$.value),
-      }).pipe(
-        switchMap(({ generatorStatus, patternStatus }) => {
-          switch (generatorStatus) {
-            case GeneratorStatusEnum.REQUEST_IN_PROGRESS:
-            case GeneratorStatusEnum.WAITING_FOR_STABILIZATION:
-              this.messagesService.warn('Hay una operación con el hardware en curso. Aguarde un momento para que finalice y vuelva a intentar.');
-              return of(false);
-          }
-          switch (patternStatus) {
-            case PatternStatusEnum.REQUEST_IN_PROGRESS:
-              this.messagesService.warn('Hay una operación con el hardware en curso. Aguarde un momento para que finalice y vuelva a intentar.');
-              return of(false);
-          }
-
-          if(generatorStatus === GeneratorStatusEnum.TIMEOUT) {
-            if(patternStatus === PatternStatusEnum.TIMEOUT) {
-              this.messagesService.error('Ocurrió un fallo crítico. Comuníquese con su proveedor.');
-              return of(true);
-            }
-          }
-
-          return of({ generatorStatus, patternStatus }).pipe(
+    return of(this.usbHandlerService.connected$.value).pipe(
+      switchMap((isConnected) => {
+        if (!isConnected) {
+          return of(true);
+        }
+        return of(false).pipe(
+          switchMap(() => forkJoin({
+            generatorStatus: of(this.generatorService.generatorStatus$.value),
+            patternStatus: of(this.patternService.patternStatus$.value),
+          }).pipe(
             switchMap(({ generatorStatus, patternStatus }) => {
-              const toTurnOff$: Observable<ResponseStatusEnum>[] = [];
-              if (generatorStatus !== GeneratorStatusEnum.TURN_OFF) {
-                toTurnOff$.push(this.generatorService.turnOff$());
+              switch (generatorStatus) {
+                case GeneratorStatusEnum.REQUEST_IN_PROGRESS:
+                case GeneratorStatusEnum.WAITING_FOR_STABILIZATION:
+                  this.messagesService.warn('Hay una operación con el hardware en curso. Aguarde un momento para que finalice y vuelva a intentar.');
+                  return of(false);
               }
-              if (patternStatus !== PatternStatusEnum.TURN_OFF) {
-                toTurnOff$.push(this.patternService.turnOff$());
+              switch (patternStatus) {
+                case PatternStatusEnum.REQUEST_IN_PROGRESS:
+                  this.messagesService.warn('Hay una operación con el hardware en curso. Aguarde un momento para que finalice y vuelva a intentar.');
+                  return of(false);
               }
-              return toTurnOff$.length === 0 ? of(true) : forkJoin<ResponseStatusEnum[]>(toTurnOff$).pipe(
-                map((responses) => {
-                  const hasError = responses.some((status) => status !== ResponseStatusEnum.ACK);
-                  return !hasError;
-                }),
-                delay(500), // esperamos para que el usuario pueda ver que se apagaron.
-              )
-            })
-          );
-        }),
-      ))
+
+              if (generatorStatus === GeneratorStatusEnum.TIMEOUT) {
+                if (patternStatus === PatternStatusEnum.TIMEOUT) {
+                  this.messagesService.error('Ocurrió un fallo crítico. Comuníquese con su proveedor.');
+                  return of(true);
+                }
+              }
+
+              return of({ generatorStatus, patternStatus }).pipe(
+                switchMap(({ generatorStatus, patternStatus }) => {
+                  const toTurnOff$: Observable<ResponseStatusEnum>[] = [];
+                  if (generatorStatus !== GeneratorStatusEnum.TURN_OFF) {
+                    toTurnOff$.push(this.generatorService.turnOff$());
+                  }
+                  if (patternStatus !== PatternStatusEnum.TURN_OFF) {
+                    toTurnOff$.push(this.patternService.turnOff$());
+                  }
+                  return toTurnOff$.length === 0 ? of(true) : forkJoin<ResponseStatusEnum[]>(toTurnOff$).pipe(
+                    map((responses) => {
+                      const hasError = responses.some((status) => status !== ResponseStatusEnum.ACK);
+                      return !hasError;
+                    }),
+                    delay(500), // esperamos para que el usuario pueda ver que se apagaron.
+                  )
+                })
+              );
+            }),
+          ))
+        );
+      }),
     );
   }
 }
