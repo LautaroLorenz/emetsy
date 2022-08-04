fb3f81b3710634b90612ceaffabc68da675f35cd
jim-llorenz <lautaro.lorenz@mercadolibre.com>
2022-08-04 13:50:21 -0300

    report create

--- src/app/components/actions/report-action/report-action.component.ts
+++ src/app/components/actions/report-action/report-action.component.ts
@@ -2,6 +2,8 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { Action, ActionComponent, Report } from 'src/app/models';
 import { ExecutionDirector } from 'src/app/services/execution-director.service';
+import { jsPDF } from 'jspdf';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-report-action',
@@ -10,7 +12,7 @@ import { ExecutionDirector } from 'src/app/services/execution-director.service';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ReportActionComponent implements ActionComponent {
-  
+
   @Input() action!: Action;
 
   get name(): string {
@@ -20,12 +22,24 @@ export class ReportActionComponent implements ActionComponent {
     return this.action.form;
   }
 
+  readonly creating$ = new BehaviorSubject<boolean>(false);
+
   constructor(
     private readonly executionDirector: ExecutionDirector,
-  ) {}
+  ) { }
 
-  downloadReport(): void {
-    const report: Report = this.executionDirector.reportEssayDirector.createReport();
-    console.log(report.html);
+  private async savePDF(html: string, name: string): Promise<jsPDF> {
+    let doc = new jsPDF();
+    await doc.html(html, { 'width': 190 });
+    return await doc.save(name);
   }
+
+  async createReport(): Promise<void> {
+    this.creating$.next(true);
+    const report: Report = this.executionDirector.reportEssayDirector.createReport();
+    await this.savePDF(report.html, 'report.pdf'); // TODO: essay name - ejecution ID
+    this.creating$.next(false);
+    return;
+  }
+
 }
