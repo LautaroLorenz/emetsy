dfda4d8e6788edd6a211088193d711797b6314e8
jim-llorenz <lautaro.lorenz@mercadolibre.com>
2022-08-05 18:38:15 -0300

    header del reporte

--- src/app/models/report/report-contrast-test.model.ts
+++ src/app/models/report/report-contrast-test.model.ts
@@ -1,37 +1,41 @@
-import { ReportBodyBuilder} from "./report-builder.model";
+import { ReportBodyBuilder } from "./report-builder.model";
 import { Report, ReportPageA4, ReportTable, ReportTbody, ReportTd, ReportTr } from "./report.model";
 
 
 export class ReportContrastTestBuilder extends ReportBodyBuilder {
 
+  private creationDate(): string {
+    const now = new Date();
+    return now.getDay() + "/" + (now.getMonth() + 1) + "/" + now.getFullYear();
+  }
+
   override produce(): Report {
+    const creationDate = this.creationDate();
     const report = new Report();
+    const pageA4 = new ReportPageA4();
+    report.pages.push(pageA4);
 
-    const pageA4_1 = new ReportPageA4();
-    const pageA4_2 = new ReportPageA4();
-    const table = new ReportTable();
-    const tbody = new ReportTbody();
-    const tr = new ReportTr();
-    const tr2 = new ReportTr();
-    const td1 = new ReportTd();
-    const td2 = new ReportTd();
+    const tableHeader = new ReportTable();
+    pageA4.add(tableHeader);
+    tableHeader.style = 'width:190mm;margin:10mm;border-collapse:collapse;font-family:Roboto;';
+    const tbodyHeader = new ReportTbody();
+    tableHeader.add(tbodyHeader);
 
-    table.style = 'background-color:pink;';
-    td1.text = 'hola';
-    td2.text = 'mundo';
+    const trHeader_1 = new ReportTr();
+    tbodyHeader.add(trHeader_1);
+    const tdHeader_1 = new ReportTd();
+    trHeader_1.add(tdHeader_1);
+    tdHeader_1.style = 'border-bottom:0.5mm solid black;text-align:right;padding-bottom:3mm;';
+    tdHeader_1.text = `<span>Reporte creado&nbsp;${creationDate}<span>`;
 
-    table.add(tbody);
-    tbody.add(tr);
-    tbody.add(tr2);
-    tr.add(td1);
-    tr.add(td2);
-    tr2.add(td2);
+    const trHeader_2 = new ReportTr();
+    tbodyHeader.add(trHeader_2);
+    const tdHeader_2 = new ReportTd();
+    trHeader_2.add(tdHeader_2);
+    tdHeader_2.style = 'padding-top:8mm;padding-bottom:15mm;text-align:center;';
+    tdHeader_2.text = `<img src="./assets/img/logo.png" />`;
 
-    pageA4_1.add(table);
-    pageA4_2.add(table);
-
-    report.pages.push(pageA4_1);
-    report.pages.push(pageA4_2);
+    
 
     return report;
   }
