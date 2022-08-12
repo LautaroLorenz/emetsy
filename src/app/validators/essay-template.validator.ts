dd21806bcb341d54b581b59c0ffe7d0bfabff0ee
jim-llorenz <lautaro.lorenz@mercadolibre.com>
2022-08-12 20:13:50 -0300

    se agregan validaciones

--- src/app/validators/essay-template.validator.ts
+++ src/app/validators/essay-template.validator.ts
@@ -1,15 +1,47 @@
 import { AbstractControl, FormGroup, ValidationErrors, ValidatorFn } from "@angular/forms";
-import { EssayErrorCodeEnum, EssayErrorMessages, EssayTemplate, EssayTemplateForm, EssayTemplateStep } from "../models";
+import { EssayErrorCodeEnum, EssayErrorMessages, EssayTemplate, EssayTemplateForm, EssayTemplateStep, StepIdEnum } from "../models";
 
 
 const name = (essayTemplate: EssayTemplate): boolean => {
   return !essayTemplate.name || essayTemplate.name.length === 0;
 };
 
-const ruleAtLeastOneStep = (steps: EssayTemplateStep[]): boolean => {
+const atLeastOneStep = (steps: EssayTemplateStep[]): boolean => {
   return steps.length === 0;
 };
 
+const preparationRequired = (steps: EssayTemplateStep[]): boolean => {
+  const hasPreparationStep = steps.some(({ step_id }) => step_id === StepIdEnum.PreparationStep);
+  const requiredPreparationSteps = [StepIdEnum.ContrastTestStep];
+  return !hasPreparationStep && steps.some(({ step_id }) => requiredPreparationSteps.includes(step_id));
+};
+
+const preparationOrderAfter = (steps: EssayTemplateStep[]): boolean => {
+  let hasError = false;
+  const requiredPreparationSteps = [StepIdEnum.ContrastTestStep];
+  let preparationStep = false;
+  for (let i = 0; i < steps.length; i++) {
+    const { step_id } = steps[i];
+    if (!preparationStep && step_id === StepIdEnum.PreparationStep) {
+      preparationStep = true;
+    }
+    if (requiredPreparationSteps.includes(step_id)) {
+      if (preparationStep) {
+        preparationStep = false;
+      } else {
+        hasError = true;
+        break;
+      }
+    }
+  }
+  return hasError;
+};
+
+const reportAtEnd = (steps: EssayTemplateStep[]): boolean => {
+  const hasReport = steps.some(({ step_id }) => step_id === StepIdEnum.ReportStep);
+  const reportIndex = steps.findIndex(({ step_id }) => step_id === StepIdEnum.ReportStep);
+  return hasReport && reportIndex < steps.length - 1;
+};
 
 export function essayTemplateValidator(): ValidatorFn {
   return (form: AbstractControl): ValidationErrors | null => {
@@ -24,8 +56,20 @@ export function essayTemplateValidator(): ValidatorFn {
       errors = { ...errors, [EssayErrorCodeEnum.name]: EssayErrorMessages[EssayErrorCodeEnum.name] };
     }
 
-    if (ruleAtLeastOneStep(steps)) {
+    if (atLeastOneStep(steps)) {
       errors = { ...errors, [EssayErrorCodeEnum.AtLeastOneStep]: EssayErrorMessages[EssayErrorCodeEnum.AtLeastOneStep] };
+    }
+
+    if (preparationRequired(steps)) {
+      errors = { ...errors, [EssayErrorCodeEnum.PreparationRequired]: EssayErrorMessages[EssayErrorCodeEnum.PreparationRequired] };
+    }
+
+    if (preparationOrderAfter(steps)) {
+      errors = { ...errors, [EssayErrorCodeEnum.PreparationOrderAfter]: EssayErrorMessages[EssayErrorCodeEnum.PreparationOrderAfter] };
+    }
+
+    if (reportAtEnd(steps)) {
+      errors = { ...errors, [EssayErrorCodeEnum.ReportAtEnd]: EssayErrorMessages[EssayErrorCodeEnum.ReportAtEnd] };
     }
 
     return Object.keys(errors) ? errors : null;
