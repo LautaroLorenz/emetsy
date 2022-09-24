fe05bc0ca62a91ab2289f62106fb8fd1f512d92a
jim-llorenz <lautaro.lorenz@mercadolibre.com>
2022-09-24 11:57:06 -0300

    número de página en el reporte y número de reporte

--- src/app/components/actions/report-action/report-action.component.ts
+++ src/app/components/actions/report-action/report-action.component.ts
@@ -1,11 +1,13 @@
-import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { FormGroup } from '@angular/forms';
-import { Action, ActionComponent, Report } from 'src/app/models';
+import { Action, ActionComponent, DateHelper, History, HistoryDbTableContext, HistoryItem, Report, ReportEssayDirector } from 'src/app/models';
 import { ExecutionDirector } from 'src/app/services/execution-director.service';
 import jsPDF from 'jspdf';
 import html2canvas from 'html2canvas';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, ReplaySubject, take, tap } from 'rxjs';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { DatabaseService } from 'src/app/services/database.service';
+import { MessagesService } from 'src/app/services/messages.service';
 
 @Component({
   selector: 'app-report-action',
@@ -13,7 +15,7 @@ import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
   styleUrls: ['./report-action.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ReportActionComponent implements ActionComponent, OnInit, AfterViewInit {
+export class ReportActionComponent implements ActionComponent, OnInit, OnDestroy {
 
   @ViewChild('container', { static: false }) container!: ElementRef;
   @Input() action!: Action;
@@ -37,23 +39,53 @@ export class ReportActionComponent implements ActionComponent, OnInit, AfterView
   }
 
   private readonly creating$ = new BehaviorSubject<boolean>(false);
+  private readonly destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
 
   constructor(
     private readonly executionDirector: ExecutionDirector,
-    private readonly sanitizer: DomSanitizer
-  ) {
+    private readonly dbServiceHistory: DatabaseService<History>,
+    private readonly sanitizer: DomSanitizer,
+    private readonly messagesService: MessagesService,
+    private readonly changeDetectorRef: ChangeDetectorRef,
+  ) { }
+
+  private createReportPreview(history: History): void {
+    const reportDirector = new ReportEssayDirector(history, this.destroyed$);
+    const report: Report = reportDirector.createReport();
+    this._report = report;
+    this._preview = report.toString();
+    this.changeDetectorRef.detectChanges();
   }
 
   ngOnInit(): void {
-    const report: Report = this.executionDirector.reportEssayDirector.createReport();
-    this._report = report;
-    this._preview = report.toString();
-  }
+    const history: History = {
+      essay: this.executionDirector.reportName,
+      saved: DateHelper.getNow(),
+    } as History;
+    history.items_raw = [];
+    this.executionDirector.steps.forEach((stepBuilder) => {
+      const historyItem: HistoryItem = {
+        essayTemplateStep: stepBuilder.essayTemplateStep,
+        reportData: stepBuilder.reportBuilder.data
+      };
+      history.items_raw.push(historyItem);
+    });
+    this.dbServiceHistory.addElementToTable$(HistoryDbTableContext.tableName, {
+      ...history,
+      items_raw: JSON.stringify(history.items_raw) as any,
+    })
+      .pipe(
+        take(1),
+        tap((id) => {
+          history.id = id;
+          this.messagesService.success('Reporte agregado al historial', 2000);
+          this.form.get('viewed')?.setValue(true);
+          this.createReportPreview(history);
+        }),
+      ).subscribe({
+        error: () => this.messagesService.error('No se pudo crear el elemento')
+      });
 
-  ngAfterViewInit(): void {
-    setTimeout(() => {
-      this.form.get('viewed')?.setValue(true);
-    }, 500);
   }
 
   async createPDF() {
@@ -71,6 +103,11 @@ export class ReportActionComponent implements ActionComponent, OnInit, AfterView
     }
     await PDF.save(`reporte - ${(new Date).getTime()}.pdf`);
     this.creating$.next(false);
+  }
+
+  ngOnDestroy() {
+    this.destroyed$.next(true);
+    this.destroyed$.complete();
   }
 
 }
--- src/app/models/report/report-director.model.ts
+++ src/app/models/report/report-director.model.ts
@@ -1,47 +1,61 @@
+import { ReplaySubject } from "rxjs";
+import { History, HistoryItem } from "../database/tables/history.model";
 import { StepBuilder } from "../steps/step-builder.model";
+import { StepConstructor } from "../steps/step-constructor.model";
 import { ReportHeaderBuilder } from "./report-header.model";
 import { Report, ReportPageA4, ReportTable } from "./report.model";
 
 export class ReportEssayDirector {
-  private steps!: StepBuilder[];
-  private name!: string;
+  private history: History;
+  private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1)
+
+  constructor(
+    history: History,
+    destroyed$: ReplaySubject<boolean>,
+  ) {
+    this.history = history;
+    this.destroyed$ = destroyed$;
+  }
+
+  private mapHistoyItemsToStepBuilders(items_raw: HistoryItem[], destroyed$: ReplaySubject<boolean>): StepBuilder[] {
+    const stepBuilders: StepBuilder[] = items_raw.map(({ essayTemplateStep }) => StepConstructor.buildStepById(essayTemplateStep.step_id, essayTemplateStep, destroyed$));
+    stepBuilders.forEach(({ reportBuilder }, index) => reportBuilder.patchValue(items_raw[index].reportData));
+    return stepBuilders;
+  }
 
   private addHeaderToPages(pages: ReportPageA4[]): void {
     const reportHeaderBuilder = new ReportHeaderBuilder();
-    if (this.name) {
-      reportHeaderBuilder.patchValue({ name: this.name })
+    if (this.history.essay) {
+      reportHeaderBuilder.patchValue({
+        name: this.history.essay,
+        reportNumber: this.history.id,
+      });
     } else {
       throw new Error('Reporte sin nombre');
     }
-    const header = reportHeaderBuilder.produce();
-
-    pages.forEach((page) => page.addHeader(header));
+    let pageNumber: number = 1;
+    pages.forEach((page) => {
+      reportHeaderBuilder.patchValue({
+        page: {
+          total: pages.length,
+          number: pageNumber++,
+        }
+      });
+      const header = reportHeaderBuilder.produce();
+      page.addHeader(header);
+    });
   }
 
-  public setSteps(steps: StepBuilder[]): void {
-    this.steps = steps;
-  }
-
-  public setName(name: string): void {
-    this.name = name;
-  }
-
-  /*
-  * TODO:
-  * - número de página
-  * - identificación del reporte (puede ser el id de ejecución)
-  */
   public createReport(): Report {
     const report: Report = new Report();
 
     const pages: ReportPageA4[] = [];
+    const stepBuilders = this.mapHistoyItemsToStepBuilders(this.history.items_raw, this.destroyed$);
     let currentPageIndex = 0;
-    this.steps.forEach(({ reportBuilder }, index) => {
-      if(index === this.steps.length - 1) {
+    stepBuilders.forEach(({ reportBuilder }, index) => {
+      if (index === stepBuilders.length - 1) {
         return; // break report step
       }
-
-      console.log(reportBuilder.requireAnEmptyPage, reportBuilder.constructor.name)
       if (pages[currentPageIndex] === undefined) {
         pages.push((new ReportPageA4()));
       }
